61b5fe480bec08a9bfdf88faa7dd7d64cb38ad63
AmitGujar <amithero3342@gmail.com>
2022-11-08 12:56:36 +0530

    error fixed

--- start.sh
+++ start.sh
@@ -1,15 +1,15 @@
-group = myLoadBalancer
-az group create -g $group northeurope
+group=myLoadBalancer
+az group create -g $group -l northeurope
 
 az network vnet create \
-    -n myVnet
-    -g $group
-    -l northeurope
-    --address-prefixes '192.168.0.0/16' \
-    --subnet-name subnet \
-    --subnet-prefixes '192.168.1.0/24' \
-
+  -n vm-vnet \
+  -g $group \
+  -l northeurope \
+  --address-prefixes '192.168.0.0/16' \
+  --subnet-name subnet \
+  --subnet-prefixes '192.168.1.0/24'
+  
 az vm availability-set create \
-    -n vm-as \
-    -l northeurope \
-    -g $group
+  -n vm-as \
+  -l northeurope \
+  -g $group
