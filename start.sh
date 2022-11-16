592bf77c5096f28652531e3bfaeb3abe79951ad2
AmitGujar <amithero3342@gmail.com>
2022-11-16 19:51:49 +0530

    new file created

--- start.sh
+++ start.sh
@@ -7,11 +7,7 @@ az network vnet create \
   -l northeurope \
   --address-prefixes '192.168.0.0/16' \
   --subnet-name subnet \
-  --subnet-prefixes '192.168.1.0/24'
-
-az network public-ip create \
-  -g $group \
-  --name myPublicIP
+  --subnet-prefixes '192.168.1.0/24' 
   
 az vm availability-set create \
   -n vm-as \
@@ -38,30 +34,3 @@ for NUM in 1 2
 do
   az vm open-port -g $group --name machine$NUM --port 80 
 done
-
-az network lb create \
-  -g $group \
-  --name myLoadBalancer \
-  --public-ip-address myPublicIP \
-  --frontend-ip-name myFrontEnd \
-  --backend-pool-name myBackendPool 
-
-az network lb probe create \
-  -g $group \
-  --lb-name myLoadBalancer \
-  --name myHealthProbe \
-  --protocol tcp \
-  --port 80 
-
-az network lb rule create \
-  -g $group \
-  --lb-name myLoadBalancer \
-  --name myHTTPRule \
-  --protocol tcp \
-  --frontend-port 80 \
-  --backend-port 80 \
-  --frontend-ip-name myFrontEnd \
-  --backend-pool-name myBackendPool \
-  --probe-name myHealthProbe \
-  --disable-outbound-snat true \
-  --idle-timeout 15 
