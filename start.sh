9d45ff6a924d43f2ab869559771649bceff40cb3
AmitGujar <amithero3342@gmail.com>
2022-11-16 16:58:29 +0530

    load balancer added

--- start.sh
+++ start.sh
@@ -8,11 +8,44 @@ az network vnet create \
   --address-prefixes '192.168.0.0/16' \
   --subnet-name subnet \
   --subnet-prefixes '192.168.1.0/24'
+
+az network public-ip create \
+  -g $group \
+  --name myPublicIP
   
 az vm availability-set create \
   -n vm-as \
   -l northeurope \
-  -g $group
+  -g $group \
+
+
+az network lb create \
+  -g $group \
+  --name myLoadBalancer \
+  --public-ip-address myPublicIP \
+  --frontend-ip-name myFrontEnd \
+  --backend-pool-name myBackendPool \
+
+az network lb probe create \
+  -g $group \
+  --lb-name myLoadBalancer \
+  --name myHealthProbe \
+  --protocol tcp \
+  --port 80 \
+
+az network lb rule create \
+  -g $group \
+  --lb-name myLoadBalancer \
+  --name myHTTPRule \
+  --protocol tcp \
+  --frontend-port 80 \
+  --backend-port 80 \
+  --frontend-ip-name myFrontEnd \
+  --backend-pool-name myBackendPool \
+  --probe-name myHealthProbe \
+  --disable-outbound-snat true \
+  --idle-timeout 15 \
+
 
 for NUM in 1 2
 do
@@ -27,10 +60,11 @@ do
     --vnet-name vm-vnet \
     --subnet subnet \
     --availability-set vm-as \
-	  --nsg vm-nsg
+	  --nsg vm-nsg \
 done
 
 for NUM in 1 2
 do
   az vm open-port -g $group --name machine$NUM --port 80
 done
+
