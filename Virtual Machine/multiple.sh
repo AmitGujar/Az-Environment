18864526a533576eef5e38bbe1fffe8f9cc61fe6
AmitGujar <amithero3342@gmail.com>
2022-11-27 18:57:41 +0530

    error fixed

--- Virtual Machine/multiple.sh
+++ Virtual Machine/multiple.sh
@@ -1,4 +1,5 @@
 group=myLinuxResource
+
 rm -r ~/.ssh
 echo "\n Generating new ssh keys.\n"
 ssh-keygen -m PEM -t rsa -b 4096
@@ -13,49 +14,43 @@ az network vnet create \
   -l centralindia \
   --address-prefixes '192.168.0.0/16' \
   --subnet-name subnet \
-  --subnet-prefixes '192.168.1.0/24' 
+  --subnet-prefixes '192.168.1.0/24'
 
 az vm availability-set create \
   -n vm-as \
   -l northeurope \
-  -g $group 
-
+  -g $group
 
 az vm create \
-    -n MasterNode \
-    -g $group \
-    -l centralindia \
-    --size Standard_B1s \
-    --image UbuntuLTS \
-    --admin-username azureuser \
-    --vnet-name vm-net \
-    --subnet subnet \
-    --generate-ssh-keys \
-    --ssh-key-values ~/.ssh/id_rsa.pub \
-
-az tag add-value --name Machine --value MasterNode
+  -n MasterNode \
+  -g $group \
+  -l centralindia \
+  --size Standard_B1s \
+  --image UbuntuLTS \
+  --admin-username azureuser \
+  --vnet-name vm-net \
+  --subnet subnet \
+  --generate-ssh-keys \
+  --ssh-key-values ~/.ssh/id_rsa.pub
 
 # creating 2 virtual machines
-for NUM in 1 2 
-do
+for NUM in 1 2; do
   az vm create \
     -n WorkerNode$NUM \
     -g $group \
     -l centralindia \
     --size Standard_B1s \
-    --image UbuntuLTS\
-    --admin-username azureuser \
+    --image UbuntuLTS --admin-username azureuser \
     --vnet-name vm-vnet \
     --subnet subnet \
     --generate-ssh-keys \
     --nsg vm-nsg \
-    --ssh-key-values ~/.ssh/id_rsa.pub \
-  
-  az tag add-value --name Machine$NUM --value WorkerNode$NUM
+    --ssh-key-values ~/.ssh/id_rsa.pub
 done
 
-# opening port 80 
-for NUM in 1 2 3
-do
-  az vm open-port -g $group --name machine$NUM --port 80 
+az vm open-port -g $group --name MasterNode --port 80
+
+# opening port 80
+for NUM in 1 2; do
+  az vm open-port -g $group --name WorkerNode$NUM --port 80
 done
