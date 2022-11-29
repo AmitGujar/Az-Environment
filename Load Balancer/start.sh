3e9ca8c8ed4ec9c453e595401b569b00623caa1f
AmitGujar <amithero3342@gmail.com>
2022-11-30 02:31:10 +0530

    location changed

--- Load Balancer/start.sh
+++ Load Balancer/start.sh
@@ -4,55 +4,52 @@ rm -r ~/.ssh
 echo "\n Generating new ssh keys.\n"
 ssh-keygen -m PEM -t rsa -b 4096
 # creating resource group for load balancer
-az group create -g $group -l northeurope
+az group create -g $group -l centralindia
 
 echo "\nInitializing Virtual Network Creation Process.\n"
 # creating virtual network
 az network vnet create \
   -n vm-vnet \
   -g $group \
-  -l northeurope \
+  -l centralindia \
   --address-prefixes '192.168.0.0/16' \
   --subnet-name subnet \
-  --subnet-prefixes '192.168.1.0/24' 
+  --subnet-prefixes '192.168.1.0/24'
 
 # setting up availability set
 az vm availability-set create \
   -n vm-as \
-  -l northeurope \
-  -g $group 
+  -l centralindia \
+  -g $group
 
 echo "\nInitializing VM Creation Process.\n"
 # creating 2 virtual machines
-for NUM in 1 2 
-do
+for NUM in 1 2; do
   az vm create \
     -n machine$NUM \
     -g $group \
-    -l northeurope \
+    -l centralindia \
     --size Standard_B1s \
-    --image UbuntuLTS\
-    --admin-username azureuser \
+    --image UbuntuLTS --admin-username azureuser \
     --vnet-name vm-vnet \
     --subnet subnet \
     --availability-set vm-as \
-	  --nsg vm-nsg \
+    --nsg vm-nsg \
     --generate-ssh-keys \
     --ssh-key-values ~/.ssh/id_rsa.pub
 done
 
-# opening port 80 
-for NUM in 1 2
-do
-  az vm open-port -g $group --name machine$NUM --port 80 
+# opening port 80
+for NUM in 1 2; do
+  az vm open-port -g $group --name machine$NUM --port 80
 done
 
 # installing nginx in vms
-for NUM in 1 2 
-do
-  az vm run-command invoke \
-    -g $group \
-    -n machine$NUM \
-    --command-id RunShellScript \
-    --script "sudo apt-get update || upgrade && sudo apt install nginx -y"
-done
+# for NUM in 1 2
+# do
+#   az vm run-command invoke \
+#     -g $group \
+#     -n machine$NUM \
+#     --command-id RunShellScript \
+#     --script "sudo apt-get update || upgrade && sudo apt install nginx -y"
+# done
