b40bfc4a0c6fa3aac1e1be4261ab405a9fb19ea2
AmitGujar <amithero3342@gmail.com>
2022-11-30 02:44:19 +0530

    version 3.0

--- Load Balancer/delete.sh
+++ Load Balancer/delete.sh
@@ -4,26 +4,26 @@ az group delete -g $group -y
 status=true
 if $status
 then
-    echo "\n ......Deleted myLoadBalancer Successfully...... \n"
+    echo "\n Deleted myLoadBalancer Successfully. \n"
 else 
-    echo "\n .......Operation Failed......."
+    echo "\n Operation Failed."
 fi
 
 az group delete --name NetworkWatcherRG -y
 status=true
 if $status
 then
-    echo "\n ......Deleted NetWorkWatcherRG........... \n"
+    echo "\n Deleted NetWorkWatcherRG.\n"
 else 
-    echo "\n ......Operation Failed......."
+    echo "\n Operation Failed"
 fi
 
-az group delete --name cloud-shell-storage-centralindia -y
-status=true
-if $status
-then 
-    echo "\n ......Deleted Terminal Storage........"
-else 
-    echo "\n .......Storage resource doesn't exists........."
-fi
+# az group delete --name cloud-shell-storage-centralindia -y
+# status=true
+# if $status
+# then 
+#     echo "\n Deleted Terminal Storage."
+# else 
+#     echo "\n Storage resource doesn't exists."
+# fi
 az group list
